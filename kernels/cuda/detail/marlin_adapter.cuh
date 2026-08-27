12c2a6399418f86381e04ae433e25522ed8a5578
ro99 <rodrigospa1991@gmail.com>
2026-08-27 14:13:36 -0300

    chore: rename third-party source directory to vendor

--- kernels/cuda/detail/marlin_adapter.cuh
+++ kernels/cuda/detail/marlin_adapter.cuh
@@ -4,5 +4,5 @@
 // subset. Keep third-party paths and Strata's required FP32 epilogue switch
 // out of the rest of the backend.
 #define STRATA_MARLIN_FP32_OUTPUT 1
-#include "../../../third_party/marlin/marlin_template.h"
+#include "../../../vendor/marlin/marlin_template.h"
 #undef STRATA_MARLIN_FP32_OUTPUT
